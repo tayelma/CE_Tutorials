539df43439dafe606c30b4ba5628fb224f3ce745
Anpah <94453946+Anpah@users.noreply.github.com>
2023-05-22 13:52:06 -0700

    Add files via upload

---
+++ my_first_python_work.py
@@ -0,0 +1,9 @@
+daily_pints = (1,2,3)
+daily_pork = {1,2,3,4,6}
+print(daily_pints)
+fruits = ['apple', 'kiwi', 'eggs', 'mangoes' 'shellfruit', 'oranges']
+print(fruits)
+x = 'hello'
+y = 'world'
+z = input(x, y, 'i have justed started programming with Azubi Africa')
+print(z)
